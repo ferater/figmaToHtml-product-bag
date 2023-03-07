8ae2f04ff37a76cb39f3736d4eb9ec027e586b93
Ferhat ER <lersmooth@gmail.com>
2023-03-07 23:05:40 +0300

    added some effects

--- src/components/ProductImage.vue
+++ src/components/ProductImage.vue
@@ -6,21 +6,14 @@ const productStore = useProductStore();
   <div class="product-image" :class="productStore.activeColor">
     <h1>michael kors</h1>
 
-        <img
+        <transition name="translate">
+          <img
           :src="`src/assets/img/women_bag_${productStore.activeColor}.png`"
           :alt="`women_bag_${productStore.activeColor}`"
+          :key="`src/assets/img/women_bag_${productStore.activeColor}.png`"
         />
+        </transition>
 
 
   </div>
 </template>
-
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 2s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
-</style>
